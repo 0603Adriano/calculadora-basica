7d8e854ed65ff4319f690e4e6a8a57ef8e934318
0603Adriano <0603adriano@gmail.com>
2021-07-07 20:27:42 -0300

    Elements added in layout

--- app/src/main/java/com/adrianomartins/calculadora/MainActivity.kt
+++ app/src/main/java/com/adrianomartins/calculadora/MainActivity.kt
@@ -2,10 +2,41 @@ package com.adrianomartins.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var num1: EditText = findViewById(R.id.editTextNum1)
+        var num2: EditText = findViewById(R.id.editTextNum2)
+
+        var btnSoma: Button = findViewById(R.id.btnSoma)
+        var btnSub: Button = findViewById(R.id.btnSub)
+        var btnMult: Button = findViewById(R.id.btnMult)
+        var btnDiv: Button = findViewById(R.id.btnDiv)
+
+        btnSoma.setOnClickListener(){
+
+        }
+
+        btnSub.setOnClickListener(){
+
+        }
+
+        btnMult.setOnClickListener(){
+
+        }
+
+        btnDiv.setOnClickListener(){
+
+        }
     }
+
 }
