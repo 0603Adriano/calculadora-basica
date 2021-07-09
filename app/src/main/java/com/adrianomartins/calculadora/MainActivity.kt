f3b4122e2adb3daea60d583f02cedd9ba5fd33d1
0603Adriano <0603adriano@gmail.com>
2021-07-09 14:58:34 -0300

    Added launcher icon

--- app/src/main/java/com/adrianomartins/calculadora/MainActivity.kt
+++ app/src/main/java/com/adrianomartins/calculadora/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.adrianomartins.calculadora
 
-import android.inputmethodservice.Keyboard
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.core.content.contentValuesOf
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         var btnElev: Button = findViewById(R.id.btnElevado)
         var btnFatorial: Button = findViewById(R.id.btnFatorar)
 
-        btnSoma.setOnClickListener(){
+        btnSoma.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
@@ -35,7 +31,7 @@ class MainActivity : AppCompatActivity() {
             exibirResultado(resultado.toString())
         }
 
-        btnSub.setOnClickListener(){
+        btnSub.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
@@ -43,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             exibirResultado(resultado.toString())
         }
 
-        btnMult.setOnClickListener(){
+        btnMult.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
@@ -51,7 +47,7 @@ class MainActivity : AppCompatActivity() {
             exibirResultado(resultado.toString())
         }
 
-        btnDiv.setOnClickListener(){
+        btnDiv.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
@@ -59,35 +55,35 @@ class MainActivity : AppCompatActivity() {
             exibirResultado(resultado.toString())
         }
 
-        btnPercent.setOnClickListener(){
+        btnPercent.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
-            var resultado = (n1/100) * n2
+            var resultado = (n1 / 100) * n2
             exibirResultado(resultado.toString())
         }
 
-        btnElev.setOnClickListener(){
-           /* var n1: Int = "${num1.text}".toInt()
-            var n2 = "${num2.text}".toInt()
+        btnElev.setOnClickListener() {
+            /* var n1: Int = "${num1.text}".toInt()
+             var n2 = "${num2.text}".toInt()
 
-            var cont = 1
+             var cont = 1
 
-            var fatora = 0
+             var fatora = 0
 
-            for (cont in 3 ){
-                fatora += n1 * n2
+             for (cont in 3 ){
+                 fatora += n1 * n2
 
-            }
+             }
 
-            exibirResultado(resultado.toString())*/
+             exibirResultado(resultado.toString())*/
         }
 
-        btnFatorial.setOnClickListener(){
+        btnFatorial.setOnClickListener() {
             var n1: Double = "${num1.text}".toDouble()
             var n2: Double = "${num2.text}".toDouble()
 
-            var resultado = (n1*100) / n2
+            var resultado = (n1 * 100) / n2
             exibirResultado(resultado.toString())
         }
     }
